64fa765bed2c7f05a2988d0cc5fbef44717294d3
ELI-LOGINS <dogbeyelias@gmail.com>
2025-01-18 00:30:00 +0000

    new file:   cart.html 	new file:   images/27-birds-jennifer-lommers.jpg 	new file:   images/img1.jpg 	new file:   images/img2.jpg 	new file:   images/img3.jpg 	new file:   images/masterBedroom001.jpg 	new file:   images/the-pilot-eric-fan-transparent.jpg 	modified:   index.html 	new file:   src/cart.js 	new file:   src/data.js 	new file:   src/main.js 	new file:   src/style.css

---
+++ src/data.js
@@ -0,0 +1,29 @@
+let shopItemsData =[{
+    id:"duverts",
+    name: "Duverts",
+    price: "95.99",
+    desc: "Luxurious and soft, our premium duvet ensures a restful night's sleep",
+    img: "images/masterBedroom001.jpg"
+
+}, 
+{
+    id:"shirts",
+    name: "Casual Shirt",
+    price: "13",
+    desc: "Effortlessly cool and comfortable, perfect for any occasion.",
+    img: "images/img1.jpg"
+},
+{
+    id:"painting",
+    name: "Sleek Black Art",
+    price: "125",
+    desc: "Vibrant hues and intricate details bring elegance and inspiration to any space.",
+    img: "images/img2.jpg"
+}, 
+{
+    id:"iphone",
+    name: "Iphone Case",
+    price: "5",
+    desc: "Sleek and stylish, this iPhone case offers ultimate protection with a touch of elegance.",
+    img: "images/img3.jpg"
+}]
